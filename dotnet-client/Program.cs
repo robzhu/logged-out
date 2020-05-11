9c13ebdeada13fac51e59d64f619299a78cc53e3
Robert Zhu <robzhu@gmail.com>
2020-05-11 13:04:27 -0700

    remove namespace

--- dotnet-client/Program.cs
+++ dotnet-client/Program.cs
@@ -4,49 +4,46 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Websocket.Client;
 
-namespace dotnet_client
+static class Program
 {
-  static class Program
+  const string LoginEndpoint = "http://localhost:9000/login";
+  const string UserID = "1234";
+  static Uri WebSocketEndpoint = new Uri("ws://localhost:9001");
+
+  static async Task Main(string[] args)
   {
-    const string LoginEndpoint = "http://localhost:9000/login";
-    const string UserID = "1234";
-    static Uri WebSocketEndpoint = new Uri("ws://localhost:9001");
+    HttpClient client = new HttpClient();
 
-    static async Task Main(string[] args)
+    client.DefaultRequestHeaders.Add("user", UserID);
+    dynamic response = JsonConvert.DeserializeObject(await client.GetStringAsync(LoginEndpoint));
+    string sessionId = response.sessionId;
+    Console.WriteLine("Obtained session ID: " + sessionId);
+
+    using (var socket = new WebsocketClient(WebSocketEndpoint))
     {
-      HttpClient client = new HttpClient();
+      await socket.Start();
 
-      client.DefaultRequestHeaders.Add("user", UserID);
-      dynamic response = JsonConvert.DeserializeObject(await client.GetStringAsync(LoginEndpoint));
-      string sessionId = response.sessionId;
-      Console.WriteLine("Obtained session ID: " + sessionId);
-
-      using (var socket = new WebsocketClient(WebSocketEndpoint))
+      socket.MessageReceived.Subscribe(msg =>
       {
-        await socket.Start();
-
-        socket.MessageReceived.Subscribe(msg =>
+        dynamic payload = JsonConvert.DeserializeObject(msg.Text);
+        if (payload["event"] == "sessionInvalidated")
         {
-          dynamic payload = JsonConvert.DeserializeObject(msg.Text);
-          if (payload["event"] == "sessionInvalidated")
-          {
-            Console.WriteLine("You have logged in elsewhere. Exiting.");
-            Environment.Exit(0);
-          }
-        });
+          Console.WriteLine("You have logged in elsewhere. Exiting.");
+          Environment.Exit(0);
+        }
+      });
 
-        socket.Send(JsonConvert.SerializeObject(new
+      socket.Send(JsonConvert.SerializeObject(new
+      {
+        action = "subscribeToSessionInvalidation",
+        args = new
         {
-          action = "subscribeToSessionInvalidation",
-          args = new
-          {
-            sessionId = sessionId
-          }
-        }));
+          sessionId = sessionId
+        }
+      }));
 
-        Console.WriteLine("Press ENTER to exit.");
-        Console.ReadLine();
-      }
+      Console.WriteLine("Press ENTER to exit.");
+      Console.ReadLine();
     }
   }
 }
